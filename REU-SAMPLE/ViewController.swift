c19e7a33b4a823780521858fdc68fd8ee8a6aff4
DanielMoraCSG <daniel@csgroup.mx>
2016-11-04 13:56:20 -0600

    Initial Commit

---
+++ REU-SAMPLE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  REU-SAMPLE
+//
+//  Created by daniel on 04/11/16.
+//  Copyright © 2016 CS Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
